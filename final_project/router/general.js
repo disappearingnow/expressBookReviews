e3cdbf338558f1713d3fabfe44ab2fac925be92b
Connor <disappearing.now@gmail.com>
2024-08-19 17:52:43 -0400

    Converted get all books path to promise

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,7 +28,21 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get("/", function (req, res) {
-  return res.status(200).json({ allBooks: books });
+  const getBooksPromise = new Promise((resolve, reject) => {
+    try {
+      resolve(books);
+    } catch (err) {
+      reject(err);
+    }
+  });
+
+  getBooksPromise
+    .then((promiseResponse) => {
+      return res.status(200).json({ allBooks: promiseResponse });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err });
+    });
 });
 
 // Get book details based on ISBN

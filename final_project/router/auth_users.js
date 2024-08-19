3818dda4503055eef3b58ffc0f6b5af4fcd0bfe9
Connor <disappearing.now@gmail.com>
2024-08-18 23:38:16 -0400

    Added user login

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -10,11 +10,40 @@ const isValid = (username) => {
 };
 
 const authenticatedUser = (username, password) => {
-  
+  let validusers = users.filter(
+    (user) => user.username === username && user.password === password
+  );
+  return validusers.length > 0;
 };
 
 //only registered users can login
-regd_users.post("/login", (req, res) => {});
+regd_users.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username or password is missing" });
+  }
+
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign(
+      {
+        data: password,
+      },
+      "access",
+      { expiresIn: 60 * 60 }
+    );
+
+    req.session.authorization = {
+      accessToken,
+      username,
+    };
+    return res.status(200).send(`User ${username} is now logged in`);
+  } else {
+    return res
+      .status(400)
+      .json({ message: "Username and password do not match any records" });
+  }
+});
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {

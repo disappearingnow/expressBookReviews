c52dff9ebecfe9ee2ab76e0992dfb6859db74352
Connor <disappearing.now@gmail.com>
2024-08-18 22:23:03 -0400

    Added status codes to responses

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -14,8 +14,8 @@ public_users.post("/register", (req, res) => {
       .json({ message: "Username and password are required" });
   }
 
-  const isUsernameTaken = isValid(username);
-  if (!isUsernameTaken) {
+  const isUsernameAvailable = isValid(username);
+  if (!isUsernameAvailable) {
     return res.status(404).json({ message: "Username is already taken" });
   }
 
@@ -27,7 +27,7 @@ public_users.post("/register", (req, res) => {
 
 // Get the book list available in the shop
 public_users.get("/", function (req, res) {
-  return res.send(JSON.stringify(books));
+  return res.status(200).send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
@@ -35,10 +35,10 @@ public_users.get("/isbn/:isbn", function (req, res) {
   const { isbn } = req.params;
 
   if (!books[isbn]) {
-    return res.send("Invalid ISBN");
+    return res.status(404).json({ message: "Invalid ISBN" });
   }
 
-  return res.send(JSON.stringify(books[isbn]));
+  return res.status(200).send(JSON.stringify(books[isbn]));
 });
 
 // Get book details based on author
@@ -51,9 +51,11 @@ public_users.get("/author/:author", function (req, res) {
   }
 
   if (bookList.length === 0) {
-    return res.send(`No books written by ${author} found`);
+    return res
+      .status(404)
+      .json({ message: `No books written by ${author} found` });
   } else {
-    return res.send(JSON.stringify(bookList));
+    return res.status(200).send(JSON.stringify(bookList));
   }
 });
 
@@ -67,9 +69,11 @@ public_users.get("/title/:title", function (req, res) {
   }
 
   if (bookList.length === 0) {
-    return res.send(`No books with the title "${title}" found`);
+    return res
+      .status(404)
+      .message({ message: `No books with the title "${title}" found` });
   } else {
-    return res.send(JSON.stringify(bookList));
+    return res.status(200).send(JSON.stringify(bookList));
   }
 });
 
@@ -78,10 +82,10 @@ public_users.get("/review/:isbn", function (req, res) {
   const { isbn } = req.params;
 
   if (!books[isbn]) {
-    return res.send("Invalid ISBN");
+    return res.status(404).json({ message: "Invalid ISBN" });
   }
 
-  return res.send(JSON.stringify(books[isbn].reviews));
+  return res.status(200).send(JSON.stringify(books[isbn].reviews));
 });
 
 module.exports.general = public_users;
